ca5fb98aef52e33bf2ac226d4b8596b4b40cc3dc
Alison Felix <felix.alison@outlook.com>
2025-04-09 14:49:23 +0100

    Duplicate lab 01 files

---
+++ Labfiles/01b-prebuild-models/Python/document-analysis.py
@@ -0,0 +1,33 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+# Store connection information
+endpoint = "<Endpoint URL>"
+key = "<API Key>"
+
+fileUri = "https://github.com/MicrosoftLearning/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true"
+fileLocale = "en-US"
+fileModelId = "prebuilt-invoice"
+
+print(f"\nConnecting to Forms Recognizer at: {endpoint}")
+print(f"Analyzing invoice at: {fileUri}")
+
+# Create the client
+
+# Analyse the invoice
+
+# Display invoice information to the user
+
+
+
+
+    customer_name = receipt.fields.get("CustomerName")
+    if customer_name:
+        print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
+
+
+    invoice_total = receipt.fields.get("InvoiceTotal")
+    if invoice_total:
+        print(f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence {invoice_total.confidence}.")
+
+print("\nAnalysis complete.\n")
